7c09a434218e1b3e98f9c12c7ca910db8a9c427a
Chamith Mendis <m.chamith@directfn.com>
2026-01-17 12:16:20 +0530

    initial project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { CartModule } from './modules/cart/cart.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { ShippingModule } from './modules/shipping/shipping.module';
+import { AdminModule } from './modules/admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ProductsModule, CartModule, OrdersModule, PaymentsModule, ShippingModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
