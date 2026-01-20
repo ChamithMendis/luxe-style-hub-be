d3f9cf020e993fa4788bd1bbf257ec14f0602d34
Chamith Mendis <m.chamith@directfn.com>
2026-01-20 06:16:30 +0530

    cors enable for developments

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
+  /* TODO: Production code */
+  // app.enableCors({
+  //   origin: ['https://your-frontend.com', 'https://another-allowed.com'],
+  //   credentials: true, // if you need cookies/auth
+  // });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
